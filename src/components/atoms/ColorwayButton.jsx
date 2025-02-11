f13012daa322207ed3f5767258fe0cd25baf0762
kalobmj <kalobmurphyjones@gmail.com>
2025-02-11 11:09:45 -0500

    main push

--- src/components/atoms/ColorwayButton.jsx
+++ src/components/atoms/ColorwayButton.jsx
@@ -5,13 +5,16 @@ import React from 'react'
     // possibly will have a border that highlights the button border when hovered.
         // the border color will be similar to the style being applied
 // it will take a handler function. this will change the underlay background and linear gradient
+// will use a map in the next component to render each button, and pass the index + 1 as its number
+    // will pass arguments during map for each colorway switch
 
-const ColorwayButton = ({ styling, handler }) => {
+const ColorwayButton = ({ styling, handler, colorNumber }) => {
     return (
         <button
-            className=''
+            className={`color-${colorNumber}`} 
+            // onClick={} 
         >
-
+            `color ${colorNumber}`
         </button>
     )
 }
