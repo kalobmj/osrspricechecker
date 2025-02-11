b7530c996a046d7a0669b556cbbd44e25ee38195
kalobmj <kalobmurphyjones@gmail.com>
2025-02-11 07:27:30 -0500

    fixing files

---
+++ src/components/atoms/GameLogo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import rsLogo from './assets/images'
+import rsLogo2 from './assets/images'
+
+// osrs main logo
+const GameLogo = () => {
+    return (
+        <img 
+            // src={} 
+            alt='osrs-logo' 
+        />
+    )
+};
+
+export default GameLogo;
