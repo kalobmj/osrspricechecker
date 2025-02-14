f2809a28d4500d1e002b1df12fc6a6d7f7da29d4
kalobmj <kalobmurphyjones@gmail.com>
2025-02-14 07:01:53 -0500

    main push

--- src/components/atoms/GameLogo.jsx
+++ src/components/atoms/GameLogo.jsx
@@ -5,9 +5,6 @@ import { getRandomImage } from '../../utils/bottomImageLoader'
 // importing all gameLogo images
 const gameLogoModules = import.meta.glob('/src/assets/images/gameLogos/**/*.png', { eager: true });
 
-console.log('gameLogoModules: ', gameLogoModules)
-console.log('gameLogoModules type: ', typeof gameLogoModules)
-
 // gameLogos object using element index for key
 // const gameLogos = Object.values(gameLogoModules).map((module, index) => ({
 //     id: index,
@@ -18,12 +15,45 @@ console.log('gameLogoModules type: ', typeof gameLogoModules)
 
 // for example we have images[0], we need to access its id and url
 
+// export default function BottomImage() {
+//     // const context = requireContext('./src/images/bottomImages', true, /\.jpg$/);
+//     // context.keys().forEach((key) => {
+//     //     const module = context(key); // import module
+//     //     console.log(module.default);
+//     // });
+//     let bottomImages;
+//     const modules = import.meta.glob('./src/images/bottomImages/**/*.jpg', { eager: true });
+//     // for (const path in modules) {
+//     //     modules[path]().then((module) => {
+//     //         console.log(path, module); // access to default export
+//     //     });
+//     // }
+    
+//     const images = Object.values(modules).map((module) => module.default);
+    
+//     console.log('Loaded images', images);
+    
+//     console.log(modules)
+//     console.log(Object.entries(modules))
+//     Promise.all(
+//         Object.entries(modules).map(([path, importer]) =>
+//             importer().then((module) => module.default)
+//         )
+//     ).then((imagePaths) => {
+//         console.log(imagePaths)
+//     });
+
 // osrs main logo
 const GameLogo = () => {
     // random number to determine which logo gets shown
     // const randomLogoNumber = randomNumber(3, 0);
 
+    console.log('gameLogoModules: ', gameLogoModules)
+    console.log('gameLogoModules type: ', typeof gameLogoModules)
+
     const randomLogo = getRandomImage[gameLogoModules];
+
+    console.log('randomLogo: ', randomLogo) // undefined
 
     return (
         <img
--- src/utils/randomNumber.js
+++ src/utils/randomNumber.js
@@ -1,3 +1,6 @@
 export const randomNumber = (max, min) => {
+    if (min === undefined) {
+        return Math.floor(Math.random() * max);
+    }
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
