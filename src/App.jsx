c13f259d5ca3917c93a254fac15c4932850e64c0
kalobmj <kalobmurphyjones@gmail.com>
2025-02-18 06:40:49 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,8 @@ function App() {
           ? `${colorScheme.url}, linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5))`
           : `${colorScheme.underlayingImage}, ${colorScheme.linearGradient}`
       }`,
-      backgroundSize: '50px 50px, auto',
-      backgroundRepeat: 'repeat, no-repeat'
+      // backgroundSize: '50px 50px, auto',
+      // backgroundRepeat: 'repeat, no-repeat'
     }))
 
   };
